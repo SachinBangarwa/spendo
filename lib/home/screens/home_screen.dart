90491473bf4a349b0f86a0e6e8417fc8fc31b36b
nkbangarwa280 <nikhilbangrwa280@gmail.com>
2025-03-22 22:05:40 +0530

    profile screen

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -24,6 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
   List days = [
     'Today',
     'Week',
+    'kuggugyuh'
     'Month',
     'Year',
     'daily'
--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,12 @@ import 'package:get/get.dart';
 
 import 'package:spendo/auth/screens/sign_up_screen.dart';
 
-import 'package:spendo/firebase_options.dart';
 
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+ // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
